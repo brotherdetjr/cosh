dd0bfc13a18310677ab1935ac8660888642b5d85
Denis Karpov <brotherdetjr@mail.ru>
2019-02-16 20:29:02 +0000

    Playing

--- cosh.go
+++ cosh.go
@@ -1,17 +1,17 @@
 package main
 
 import (
-	"bufio"
 	"github.com/brotherdetjr/goja"
+	"os"
 	"os/exec"
 )
 
 func main() {
 	vm := goja.New()
 	global := vm.GlobalObject()
-	global.Set("nunjucks", evalResource("embedded/nunjucks.js", vm))
+	_ = global.Set("nunjucks", evalResource("embedded/nunjucks.js", vm))
 	coffee := evalResource("embedded/coffee-script.js", vm)
-	global.Set("coffee", coffee)
+	_ = global.Set("coffee", coffee)
 	compile := coffee.ToObject(vm).Get("compile").Export().(func(goja.FunctionCall) goja.Value)
 	arg := goja.FunctionCall{
 		This: vm.GlobalObject(),
@@ -31,45 +31,14 @@ return "f({{ a }}, {{ b }}) = {{ f }}".render a: a, b: b, f: f(a, b)
 		println(value.String())
 	}
 
-	cmd1 := exec.Command("echo", "Hello World!")
-	cmd2 := exec.Command("sed", `s/o/x/g`)
+	c1 := exec.Command("echo", "hello world\ndiso")
+	c2 := exec.Command("sed", "s/o/x/g")
 
-	outPipe, err := cmd1.StdoutPipe()
-	if err != nil {
-		panic(err)
-	}
-	inPipe, err := cmd2.StdinPipe()
-	if err != nil {
-		panic(err)
-	}
-	resultPipe, err := cmd2.StdoutPipe()
-	if err != nil {
-		panic(err)
-	}
-	scanner1 := bufio.NewScanner(outPipe)
-	scanner2 := bufio.NewScanner(resultPipe)
-
-	go func() {
-		for scanner1.Scan() {
-			if _, err := inPipe.Write(scanner1.Bytes()); err != nil {
-				panic(err)
-			}
-		}
-		inPipe.Close()
-	}()
-
-	go func() {
-		for scanner2.Scan() {
-			print(scanner2.Text())
-		}
-	}()
-
-	cmd2.Start()
-	cmd1.Start()
-
-	if err := cmd2.Wait(); err != nil {
-		panic(err)
-	}
+	c2.Stdin, _ = c1.StdoutPipe()
+	c2.Stdout = os.Stdout
+	_ = c2.Start()
+	_ = c1.Start()
+	_ = c2.Wait()
 }
 
 func evalResource(resourceName string, vm *goja.Runtime) goja.Value {
