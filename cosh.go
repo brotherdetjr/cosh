3990394ebc0043f7f1707eb96a49231f142cb7ba
Denis Karpov <brotherdetjr@mail.ru>
2019-02-16 10:44:19 +0000

    Playing

--- cosh.go
+++ cosh.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"bufio"
 	"github.com/brotherdetjr/goja"
+	"os/exec"
 )
 
 func main() {
@@ -14,11 +16,60 @@ func main() {
 	arg := goja.FunctionCall{
 		This: vm.GlobalObject(),
 		Arguments: []goja.Value{
-			goja.NewStringValue("({x, y}).x = {x: 42}"),
+			goja.NewStringValue(`
+f = (x, y) => x + y
+[a, b] = [3, 4]
+return "f({{ a }}, {{ b }}) = {{ f }}".render a: a, b: b, f: f(a, b) 
+`),
 		},
 	}
-	println(compile(arg).String())
+	js := compile(arg).String()
+	println(js)
+	if value, err := vm.RunString(js); err != nil {
+		panic(err)
+	} else {
+		println(value.String())
+	}
 
+	cmd1 := exec.Command("echo", "Hello World!")
+	cmd2 := exec.Command("sed", `s/o/x/g`)
+
+	outPipe, err := cmd1.StdoutPipe()
+	if err != nil {
+		panic(err)
+	}
+	inPipe, err := cmd2.StdinPipe()
+	if err != nil {
+		panic(err)
+	}
+	resultPipe, err := cmd2.StdoutPipe()
+	if err != nil {
+		panic(err)
+	}
+	scanner1 := bufio.NewScanner(outPipe)
+	scanner2 := bufio.NewScanner(resultPipe)
+
+	go func() {
+		for scanner1.Scan() {
+			if _, err := inPipe.Write(scanner1.Bytes()); err != nil {
+				panic(err)
+			}
+		}
+		inPipe.Close()
+	}()
+
+	go func() {
+		for scanner2.Scan() {
+			print(scanner2.Text())
+		}
+	}()
+
+	cmd2.Start()
+	cmd1.Start()
+
+	if err := cmd2.Wait(); err != nil {
+		panic(err)
+	}
 }
 
 func evalResource(resourceName string, vm *goja.Runtime) goja.Value {
@@ -32,7 +83,3 @@ func evalResource(resourceName string, vm *goja.Runtime) goja.Value {
 		}
 	}
 }
-
-/*func evalCoffee(script string) goja.Value {
-
-}*/
